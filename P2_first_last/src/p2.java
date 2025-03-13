838b6510fa62c8741072a926ed4833bf069203f9
Brandon Tran <141108900+bug3x@users.noreply.github.com>
2025-03-13 13:17:22 -0700

    Update p2.java

--- P2_first_last/src/p2.java
+++ P2_first_last/src/p2.java
@@ -1,9 +1,30 @@
+import java.util.*;
+import java.io.*;
 
 public class p2 {
-
+	
+	public static String quadSearch(String ele) {
+		// read all NWSE paths
+		if()
+		return "";
+	}
+	
+	public static Tile[][][] queueSearch() {
+		Queue searchList = new LinkedList();
+		firstPosition(searchList);
+		return null;
+	}
+	
+	public static Queue firstPosition(Queue queue) {
+		Tile[][][] tile = MapReader.readMap("txt_map1.txt");
+		Tile[][][] pos = MapReader.startPosition(tile, 0);
+		queue.add(pos);
+		return queue;
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("p2");
+		firstPosition(/*some queue*/);
 	}
 
 }
